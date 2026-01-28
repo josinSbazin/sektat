1ea352ceb942828b043434d60a4fa19f9b1c3ace
rhanza <rhanza@bk.ru>
2026-01-28 15:35:06 +0300

    Fix base URL trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://josinsbazin.github.io',
-  base: '/sektat',
+  base: '/sektat/',
 
   i18n: {
     defaultLocale: 'ru',
